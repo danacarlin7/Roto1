763a58db1905210ba72d94068a42d33f5cddab4e
HirenParekh <hiren0707@gmail.com>
2017-06-05 22:29:56 +0530

    Project Structure Updated.

--- src/app/core/forms/member.ts
+++
@@ -1,6 +0,0 @@
-export interface Member {
-	email;
-	first_name;
-	last_name;
-	password;
-}
--- src/app/core/models/filter-criteria.model.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Hiren on 04-05-2017.
- */
-
-export interface FilterCriteria {
-  key:string;
-  value:any;
-}
