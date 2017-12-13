450955915a47830a95cd0d37458671f1fb36ea37
Argonautic <jonathan.zhou93@gmail.com>
2017-12-13 16:26:50 -0500

    added paper admin draft and added pro-admin dependencies to package.json

---
+++ src/app/paper-admin/admin.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from "@angular/core";
+
+import { AdminComponent } from "./admin.component";
+import { AdminRoutingModule } from "./admin-routing.module";
+import { SidebarModule } from "./components/sidebar/sidebar.module";
+import { FooterModule } from "./components/shared/footer/footer.module";
+import { NavbarModule} from "./components/shared/navbar/navbar.module";
+import { FixedPluginModule} from "./components/shared/fixedplugin/fixedplugin.module";
+import { NguiMapModule} from "@ngui/map";
+
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { UserComponent } from "./components/user/user.component";
+import { TableComponent } from "./components/table/table.component";
+import { TypographyComponent } from "./components/typography/typography.component";
+import { IconsComponent } from "./components/icons/icons.component";
+import { MapsComponent } from "./components/maps/maps.component";
+import { NotificationsComponent } from "./components/notifications/notifications.component";
+import { UpgradeComponent } from "./components/upgrade/upgrade.component";
+import { SharedModule } from "../shared/shared.module";
+import { MembersAdminComponent } from "./members-admin/members-admin.component";
+import { AdminDashboardService } from "./services/admin-dashboard.service";
+import { MembersResolver } from "app/paper-admin/services/members-resolver.service";
+import { MembershipPlanService } from './services/membership-plan.service';
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    DashboardComponent,
+    UserComponent,
+    TableComponent,
+    TypographyComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+    UpgradeComponent,
+    MembersAdminComponent
+  ],
+  imports: [
+    AdminRoutingModule,
+    SharedModule,
+    SidebarModule,
+    NavbarModule,
+    FooterModule,
+    FixedPluginModule,
+    NguiMapModule.forRoot({apiUrl: "https://maps.google.com/maps/api/js?key=AIzaSyBr-tgUtpm8cyjYVQDrjs8YpZH7zBNWPuY"}),
+  ],
+  exports: [
+    AdminComponent
+  ],
+  providers: [
+    AdminDashboardService,
+    MembersResolver,
+    MembershipPlanService
+  ],
+})
+export class AdminModule { }
---
+++ src/app/paper-admin/members-admin/members-admin.component.ts
@@ -0,0 +1,142 @@
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from "@angular/core";
+import { AdminDashboardService } from "../services/admin-dashboard.service";
+import { Overlay } from "angular2-modal";
+import { overlayConfigFactory } from "angular2-modal";
+import { Modal, BSModalContext } from "angular2-modal/plugins/bootstrap";
+import { MembershipPlanService } from "../services/membership-plan.service";
+import moment = require("moment");
+import "../../../assets/newAdmin/js/datetime-moment.js";
+
+declare var $: any;
+
+@Component({
+  selector: "app-members-admin",
+  templateUrl: "./members-admin.component.html",
+  styleUrls: ["./members-admin.component.css"]
+})
+export class MembersAdminComponent implements OnInit {
+  @ViewChild("subscribeTemplateRef") private subscribeTemplateRef: TemplateRef<any>;
+  @ViewChild("unsubscribeTemplateRef") private unsubscribeTemplateRef: TemplateRef<any>;
+  @ViewChild("deleteTemplateRef") private deleteTemplateRef: TemplateRef<any>;
+
+  private headerRow = ["Name", "Email", "Is Subscribed", "Created On", "Last Subscription", "Actions"];
+  private allMembers: Object[];
+
+  private modalData;
+  private dialogRef;
+  private table;
+  private subscriptionPlans: Object[];
+
+  constructor(
+    private adminDashboardService: AdminDashboardService,
+    private membershipPlanService: MembershipPlanService,
+    private modal: Modal,
+    private vcRef: ViewContainerRef,
+    private overlay: Overlay
+  ) {
+    modal.overlay.defaultViewContainer = vcRef;
+  }
+
+  ngOnInit() {
+    this.adminDashboardService.getMembers().subscribe(
+      members => {
+        this.allMembers = members.data.map(member => {
+          let last_active = null;
+
+          const created_at = moment(member.created_at).format("MMM D YYYY");
+          if (member.last_active) {
+            last_active = moment(member.last_active).format("MMM D YYYY");
+          }
+
+          return {
+            name: member.first_name + " " + member.last_name,
+            email: member.email,
+            isSubscribe: member.is_subscribe,
+            createdAt: created_at,
+            lastSubscription: last_active,
+            id: member._id,
+            subscriptions: member.subscriptions.filter(subscription => subscription.is_plan_active === true)
+          };
+        });
+
+        setTimeout(() => this.setUpDatatable(), 0);
+      }
+    );
+
+    this.membershipPlanService.retrieveMembershipPlans().subscribe(
+      plans => {
+        this.subscriptionPlans = [plans.data[1], plans.data[2]];
+      }
+    );
+  }
+
+  setUpDatatable() {
+    if (this.table) {
+      this.table.destroy();
+    }
+
+    $.fn.dataTable.moment("MMM D YYYY");
+
+    this.table = $("#datatables").DataTable({
+      "pagingType": "full_numbers",
+      "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+      responsive: true,
+      language: {
+      search: "_INPUT_",
+        searchPlaceholder: "Search records",
+      },
+      "order": [[ 3, "desc" ]]
+    });
+  }
+
+  openModal(data: Object, templateRef: TemplateRef<any>) {
+    console.log(data);
+    this.modalData = data;
+    this.modal.open(templateRef, overlayConfigFactory({isBlocking: false}, BSModalContext)).then(
+      dialog => this.dialogRef = dialog
+    );
+  }
+
+  subscribeMember(form) {
+    console.log(form.value.planID);
+    console.log(this.modalData.id);
+    console.log(typeof this.modalData.createdAt);
+  }
+
+  unsubscribeMember(form) {
+    this.adminDashboardService.unsubscribePlan(form.value.planID, false, this.modalData.id).subscribe(
+      response => {
+        console.log(response);
+          alert(response.message);
+          this.dialogRef.close(true);
+      }
+    );
+  }
+
+  deleteUser(id: String) {
+    this.adminDashboardService.deleteMember(id).subscribe(
+      response => {
+        console.log(response);
+        alert(response.message);
+        this.dialogRef.close(true);
+
+        const $tr = $(`#${id}`);
+        this.table.row($tr).remove().draw();
+      },
+      error => {
+        console.log(error);
+        alert(error.message);
+      }
+    );
+  }
+
+  downloadMembers() {
+    if (!this.allMembers) {
+      alert("Wait for members to load");
+      return;
+    }
+
+    this.adminDashboardService.downloadCSV(this.allMembers, "allMembers.csv");
+  }
+
+}
