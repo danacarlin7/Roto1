dd2c2c8ff9c09350faf36f02cd481a6826857074
Argonautic <jonathan.zhou93@gmail.com>
2018-01-08 14:00:46 -0500

    Removed example-only links in sidebar for production

--- src/app/new-admin/admin-routing.module.ts
+++ src/app/new-admin/admin-routing.module.ts
@@ -1,13 +1,6 @@
 import { RouterModule, Routes } from "@angular/router";
 
 import { DashboardComponent }   from "./components/dashboard/dashboard.component";
-import { UserComponent }   from "./components/user/user.component";
-import { TableComponent }   from "./components/table/table.component";
-import { TypographyComponent }   from "./components/typography/typography.component";
-import { IconsComponent }   from "./components/icons/icons.component";
-import { MapsComponent }   from "./components/maps/maps.component";
-import { NotificationsComponent }   from "./components/notifications/notifications.component";
-import { UpgradeComponent }   from "./components/upgrade/upgrade.component";
 import { NgModule } from "@angular/core";
 import { AdminComponent } from "./admin.component";
 import { MembersAdminComponent } from "./members-admin/members-admin.component";
@@ -47,34 +40,6 @@ export const routes: Routes = [
         path: "promotions",
         component: PromotionsComponent
       },
-      {
-        path: "user",
-        component: UserComponent
-      },
-      {
-        path: "table",
-        component: TableComponent
-      },
-      {
-        path: "typography",
-        component: TypographyComponent
-      },
-      {
-        path: "icons",
-        component: IconsComponent
-      },
-      {
-        path: "maps",
-        component: MapsComponent
-      },
-      {
-        path: "notifications",
-        component: NotificationsComponent
-      },
-      {
-        path: "upgrade",
-        component: UpgradeComponent
-      }
     ]
   }
 ];
--- src/app/new-admin/components/sidebar/sidebar.component.ts
+++ src/app/new-admin/components/sidebar/sidebar.component.ts
@@ -12,13 +12,7 @@ export interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: "dashboard", title: "Dashboard",  icon: "ti-panel", class: "" },
     { path: "members", title: "Members",  icon: "ti-user", class: "" },
-    { path: "promotions", title: "Promotions", icon: "ti-gift", class: ""},
-    { path: "user", title: "User Profile",  icon: "ti-user", class: "" },
-    { path: "table", title: "Table List",  icon: "ti-view-list-alt", class: "" },
-    { path: "typography", title: "Typography",  icon: "ti-text", class: "" },
-    { path: "icons", title: "Icons",  icon: "ti-pencil-alt2", class: "" },
-    { path: "maps", title: "Maps",  icon: "ti-map", class: "" },
-    { path: "notifications", title: "Notifications",  icon: "ti-bell", class: "" },
+    { path: "promotions", title: "Promotions", icon: "ti-gift", class: ""}
 ];
 
 @Component({
