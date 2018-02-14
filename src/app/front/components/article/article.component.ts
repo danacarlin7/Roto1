ea6b8136b54432a140cfd9178d70cbf23e8c935c
Mrugank <mrugank_1993@outlook.com>
2018-02-14 15:31:12 +0530

    article test meta tags

--- src/app/front/components/article/article.component.ts
+++ src/app/front/components/article/article.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ArticleService} from "../../services/article.service";
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-article',
@@ -9,7 +10,7 @@ import {ArticleService} from "../../services/article.service";
 })
 export class ArticleComponent implements OnInit {
 
-  constructor(private activatedR:ActivatedRoute,private router:Router , private articleService:ArticleService) {
+  constructor(private activatedR:ActivatedRoute,private router:Router , private articleService:ArticleService, private meta: Meta) {
   }
 
   id:any;
@@ -23,6 +24,27 @@ export class ArticleComponent implements OnInit {
         this.articleService.fetchPost(this.id).subscribe(
           response => {
             this.article = response;
+            console.log(this.article);
+
+            this.meta.addTag({ name: 'twitter:title', content: this.article.title.rendered });
+
+          }
+        );
+
+        this.articleService.fetchMedia({parent : 4253}).subscribe(
+          response => {
+            // this.article = response;
+            // console.log(this.article);
+            console.log(response[0].source_url);
+            // this.meta.addTag({ name: 'author', content: 'How to use Angular 4 meta service' });
+             // this.meta.addTag({ name: 'author', content: 'talkingdotnet' });
+             if(response && response[0].source_url)
+              this.meta.addTag({ name: 'twitter:image', content: response[0].source_url });
+              // <meta name="twitter:card" content="summary" />
+              // <meta name="twitter:site" content="@RotoProsDFS" />
+              // <meta name="twitter:title" content="Small Island Developing States Photo Submission" />
+              // <meta name="twitter:description" content="View the album on Flickr." />
+              // <meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
           }
         );
       }
