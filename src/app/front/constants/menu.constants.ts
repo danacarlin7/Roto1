57aa3f37275316e1969b35b6d3d81535e8eb5ddb
Dana Carlin <dana@rotopros.com>
2018-03-16 16:59:21 +0000

    Replace menu.constants.ts

--- src/app/front/constants/menu.constants.ts
+++ src/app/front/constants/menu.constants.ts
@@ -4,13 +4,24 @@
 
 
 export const NewsTabConstants = {
-  NFL: 'NFL',
-  NBA: 'NBA',
-  MLB: 'MLB'
+  MLB: "MLB",
+  MMA: "MMA",
+  NASCAR: "NASCAR",
+  NBA: "NBA",
+  NFL: "NFL",
+  NHL: "NHL",
+  PGA: "PGA",
+  SOCCER: "SOCCER",
+
 };
 
-export const NewsTabs:{value:string,label:string}[] = [
-  {value: NewsTabConstants.NFL, label: 'NFL'},
-  {value: NewsTabConstants.NBA, label: 'NBA'},
-  {value: NewsTabConstants.MLB, label: 'MLB'}
+export const NewsTabs: {value: string, label: string}[] = [
+  {value: NewsTabConstants.MMA, label: "MMA"},
+  {value: NewsTabConstants.MLB, label: "MLB"},
+  {value: NewsTabConstants.NBA, label: "NBA"},
+  {value: NewsTabConstants.NFL, label: "NFL"},
+  {value: NewsTabConstants.NHL, label: "NHL"},
+  {value: NewsTabConstants.PGA, label: "PGA"},
+  {value: NewsTabConstants.NASCAR, label: "NASCAR"},
+  {value: NewsTabConstants.SOCCER, label: "SOCCER"}
 ];
