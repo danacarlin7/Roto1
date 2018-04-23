36ec1016c9381b56fcfd20be96e2d55238e0617a
Danacarlin7 <danacarlin7@gmail.com>
2018-04-23 18:55:47 -0400

    added twitter widget

---
+++ src/app/front/components/plan/plan.component.ts
@@ -0,0 +1,24 @@
+import {Component} from "@angular/core";
+import {Router} from "@angular/router";
+import {FrontService} from "../../services/front.service";
+import {AuthService} from "../../../shared/services/auth.service";
+
+@Component({
+  selector: "rp-plan-component",
+  templateUrl: "./plan.component.html",
+  styleUrls: ["./plan.component.css"]
+})
+export class PlanComponent {
+  plans;
+
+  isLogin: boolean;
+
+ constructor(private router: Router, private frontService: FrontService, private authService: AuthService) {
+   // this.plans = this.frontService.getDummyPlans();
+    this.isLogin = this.authService.isLoggedIn();
+  }
+
+ onBtnSubscribeClick() {
+    this.router.navigate(["/signup"]);
+  }
+}
