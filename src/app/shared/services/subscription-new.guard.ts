b23edb351bfb8c010bdee4f128b5876ed09de1be
Mrugank <mrugank_1993@outlook.com>
2018-05-12 13:13:31 +0530

    logo and article issue fixed

--- src/app/shared/services/subscription-new.guard.ts
+++ src/app/shared/services/subscription-new.guard.ts
@@ -1,77 +1,48 @@
 /**
  * Created by Hiren on 30-08-2017.
  */
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import {
   CanActivate, Route, Router, ActivatedRouteSnapshot, ActivatedRoute, RouterStateSnapshot,
   CanActivateChild
 } from "@angular/router";
-import {AuthService} from "./auth.service";
+import { AuthService } from "./auth.service";
 import { ArticleService } from "../../front/services/article.service";
 
 @Injectable()
 export class SubscriptionNewGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router, private activatedRoute: ActivatedRoute, private articleService: ArticleService) {
-    if(localStorage.getItem('reloadOn') === "1"){
+    if (localStorage.getItem('reloadOn') === "1") {
       localStorage.setItem('free', "0");
       localStorage.setItem('reloadOn', "0");
     }
   }
 
-  checkArticle(id, callback){
-    this.articleService.fetchPost(id).subscribe(
-      response => {
-        // this.article = response;
-        // console.log(response.categories[0]);
-        this.articleService.fetchFreeCategory().subscribe(
-          responses => {
-            let cat_cnt = 0;
-            let isFree = false;
-
-            for (let value of response.categories) {
-              console.log(value, responses);
-              cat_cnt++;
-
-              if(value === responses[0].id){
-                isFree = true;
-              }
-
-              if(response.categories.length == cat_cnt)
-                callback(isFree);
-            }
-
-          }
-        );
-
-      }
-    );
-  }
-
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // console.log(route.params.id);
     let that = this;
-    console.log('local'+ localStorage.getItem('free'));
-    if(localStorage.getItem('free') == "1"){
+    console.log('local' + localStorage.getItem('free'));
+    if (localStorage.getItem('free') == "1") {
       return true;
     } else if (this.authService.isLoggedIn()) {
       if (this.authService.isSubscriber(true)) {
         return true;
       } else {
-        this.router.navigate(['/subscribe'], {queryParams: {redirect: state.url}});
+        this.router.navigate(['/subscribe'], { queryParams: { redirect: state.url } });
         return false;
       }
     } else {
       console.log("else here", route.params.id);
-      this.checkArticle(route.params.id, function(resp){
+      this.authService.checkArticle(route.params.id, function(resp) {
         console.log("check article", resp);
-        if(resp){
+        if (resp) {
           localStorage.setItem('free', "1");
-          that.router.navigate(['/articles/'+route.params.id]);
+          that.router.navigate(['/articles/' + route.params.id]);
           localStorage.setItem('reloadOn', "1");
           return true;
         } else {
           localStorage.setItem('free', "0");
-          that.router.navigate(['/login'], {queryParams: {redirect: state.url}});
+          that.router.navigate(['/login'], { queryParams: { redirect: state.url } });
           return false;
         }
       });
